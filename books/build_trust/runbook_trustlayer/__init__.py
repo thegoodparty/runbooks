4cb69362528c4d7a654eebb63d152314908a9c46
Melecia <melecia@goodparty.org>
2026-05-05 15:38:26 -0400

    moved old data

--- books/build_trust/runbook_trustlayer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""QA layer for runbook-generated governance briefings."""
-
-from .pipeline import RunbookQAPipeline, validate_output_folder
-
-__all__ = ["RunbookQAPipeline", "validate_output_folder"]
-
